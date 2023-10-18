53820fb5fb17695551fab92b8add362265abc692
Cihat KOÇAK <cihat.kocak@kodris.com>
2023-10-18 13:39:54 +0300

    first commit

---
+++ python/strings.py
@@ -0,0 +1,25 @@
+my_string = "Welcome to Python!"
+another_string = 'The bright red fox jumped the fence.'
+a_long_string = '''This is a
+multi-line string. It covers more than
+one line'''
+
+my_number = 123
+
+my_string = str(my_number)
+
+x = int("123")
+
+my_unicode_string = u"This is unicode!"
+
+my_string = "This is a string!"
+my_string.upper()
+"This is a string!".upper()
+
+# dir(my_string)
+
+# help(my_string.upper())
+
+print(my_string[2:])
+
+print("Python is as simple as {0}, {1}, {2}".format("a", "b", "c"))
